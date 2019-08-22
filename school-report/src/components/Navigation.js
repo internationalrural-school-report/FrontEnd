2ecdd4da6e7c7a69f737143f0a6808e05259ca0c
William M <wmooney1984@gmail.com>
2019-08-21 23:09:17 -0400

    NavLinks Setup on Navigation

--- school-report/src/components/Navigation.js
+++ school-report/src/components/Navigation.js
@@ -1,1 +1,21 @@
-// For our Nav Component
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Navigation() {
+  return (
+    <nav>
+      <NavLink exact to='/' style={{ padding: '0 5px' }}>
+        Home
+      </NavLink>
+      <NavLink to='/issues' style={{ padding: '0 5px' }}>
+        Issue Log
+      </NavLink>
+      <NavLink to='/login' style={{ padding: '0 5px' }}>
+        Login
+      </NavLink>
+      <NavLink to='/register' style={{ padding: '0 5px' }}>
+        Register
+      </NavLink>
+    </nav>
+  );
+}
