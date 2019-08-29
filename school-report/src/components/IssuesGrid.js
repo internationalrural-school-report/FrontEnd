77132530dd74dc811a1bdb73a6230d28376907dc
Nick <hansen.nick95@gmail.com>
2019-08-28 22:20:37 -0700

    Finished sorting by status

--- school-report/src/components/IssuesGrid.js
+++ school-report/src/components/IssuesGrid.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Issue from './Issue';
 import { axiosWithAuth } from './axiosWithAuth';
+import axios from 'axios'
 
 function IssuesGrid() {
   const [issues, setIssues] = useState([]);
+  const [statusList, setStatusList] = useState([]);
+  const [sortBy, setSortBy] = useState();
 
   useEffect(() => {
     axiosWithAuth()
@@ -16,15 +19,44 @@ function IssuesGrid() {
   .catch(res => {
     console.log('Error', res);
   });
-  }, [])
+  }, []);
 
+  useEffect(() => {
+    axios.get('https://irsr-be-dev.herokuapp.com/public/issue-status')
+      .then( res => setStatusList(res.data))
+      .catch( err => console.log(err))
+  }, []);
+
+  const statusDrop = statusList.map( (status) => {
+    return (
+      <option key={status.name} value={status.id}>{status.name}</option>
+    )
+  });
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    console.log(sortBy)
+  }
+
+  const handleChange = (ev) => {
+    setSortBy(ev.target.value);
+  }
   //
   if (localStorage.getItem('token')) {
 
     return (
       <div>
+        <form onSubmit={handleSubmit}>
+          <select name='status' onChange={(ev) => handleChange(ev)}>
+            <option>Sort by Issue Status</option>
+            {statusDrop}
+          </select>
+          <button type='submit'>Sort</button>
+        </form>
         {issues.map(issue => {
+          if (issue.status_id == sortBy) {
           return <Issue obj={issue} key={issue.id} />;
+          }
         })}
       </div>
     );
