55b7d552fa1d04747e46d957a0d00ce38a47282e
William M <wmooney1984@gmail.com>
2019-08-28 21:12:10 -0400

    Home Page Displaying Issues

--- school-report/src/App.js
+++ school-report/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 import AppRouter from './components/AppRouter';
-import AddIssueForm from './components/AddIssueForm';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
       <AppRouter />
-      <AddIssueForm />
     </div>
   );
 }
