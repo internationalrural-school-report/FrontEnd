f45739dd25a548af85ceef18f9dc7dd65e6518aa
William M <wmooney1984@gmail.com>
2019-08-29 21:28:49 -0400

    Removed unnecessary files and added a dependency

--- school-report/src/index.js
+++ school-report/src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import {positions, Provider as AlertProvider} from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
-
-const options = {position: positions.MIDDLE}
 
 ReactDOM.render(
   <Router>
-    <AlertProvider template={AlertTemplate} {...options}>
-      <App />
-    </AlertProvider>
+    <App />
   </Router>,
   document.getElementById('root')
 );
